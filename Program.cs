fa325512aa310260d465c406ffbcfa500d0bb8e2
comkar999 <31873952+comkar999@users.noreply.github.com>
2017-09-17 15:44:35 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,81 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DelegatesAssignment3
+namespace ConsoleApplication1
 {
-    public delegate double AdditionDelegate(double number);
-    public delegate double SubstractionDelegate(double number);
-    public delegate double MultiplicationDelegate(double number);
-    public delegate double MCDelegate(double number);
-    class Program
-    {
-        static void Main(string[] args)
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+   
+        public delegate double AdditionDelegate(double number);
+        public delegate double SubstractionDelegate(double number);
+        public delegate double MultiplicationDelegate(double number);
+        public delegate void MCDelegate();
+        class Program
         {
-
-            Console.WriteLine("Enter the operation you wanna perform");
-            string operation=  Console.ReadLine();
-            switch(operation)
+            static void Main(string[] args)
             {
-                case "Addition":
-                    Program p1 = new Program();
-                    AdditionDelegate ad = new AdditionDelegate(p1.Addition);
-                    Console.WriteLine("Enter the number for addition operation");
-                    double number1= double.Parse(Console.ReadLine());
-                    Console.WriteLine(ad(number1));
-                    break;
 
-                case "Substraction":
-                    Program p2 = new Program();
-                    SubstractionDelegate sb = new SubstractionDelegate(p2.Substraction);
-                    Console.WriteLine("enter the number for substraction operation");
-                    double number2 = double.Parse(Console.ReadLine());
-                    Console.WriteLine(sb(number2));
-                    break;
-                case "Multiplication":
-                    Program p3 = new Program();
-                    MultiplicationDelegate md = new MultiplicationDelegate(p3.Multiplication);
-                    Console.WriteLine("enter the number for multipication operation");
-                    double number3 = double.Parse(Console.ReadLine());
-                    Console.WriteLine(md(number3));
-                    break;
-                case "MultipleOperations":
-                    Program p4 = new Program();
-                    MCDelegate mc1 = new MCDelegate(p4.Addition);
-                    MCDelegate mc2 = new MCDelegate(p4.Substraction);
-                    MCDelegate mc3 = new MCDelegate(p4.Multiplication);
-                    MCDelegate mc4 = mc1 + mc2 + mc3;
-                    Console.WriteLine("enter the number for the multiple operation");
-                    double number4 = double.Parse(Console.ReadLine());
-                    Console.WriteLine(mc4(number4));
-                    break;
-
-                default:
-
-                    Console.WriteLine("default case executed");
-                    break;
-
+                
+                
+                   
+                        Program p4 = new Program();
+                        MCDelegate mc1 = new MCDelegate(p4.Addition);
+                        MCDelegate mc2 = new MCDelegate(p4.Substraction);
+                        MCDelegate mc3 = new MCDelegate(p4.Multiplication);
+                        MCDelegate mc4 = mc1 + mc2 + mc3;
+                        
+                        mc4();
+                        Console.ReadLine();
 
             }
 
-            Console.ReadLine();
+            public void Addition()
+            {
+                Console.WriteLine("Addition is performed");
+            }
+
+            public void Substraction()
+            {
+                Console.WriteLine("Substraction is performed");
+            }
+
+            public void Multiplication()
+            {
+               Console.WriteLine( "Multiplication is performed");
+            }
+
 
         }
-
-        public double Addition(double number)
-        {
-            return number + number;
-        }
-
-        public double Substraction(double number)
-        {
-            return number - 2;
-        }
-
-        public double Multiplication(double number)
-        {
-            return number * 2;
-        }
-
-       
     }
-}
+
+
